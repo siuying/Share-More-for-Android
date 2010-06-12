f8db258b5bf4ed607ef6a8a3ff823b36e730a71b
siuying <siu.ying@gmail.com>
2010-06-12 11:15:54 +0800

    add UI for configuration

--- gen/hk/ignition/share/R.java
+++ gen/hk/ignition/share/R.java
@@ -14,8 +14,15 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
-        public static final int error_find_title=0x7f030002;
-        public static final int share_with_title=0x7f030001;
+        public static final int app_name=0x7f040000;
+        public static final int bitly_enable=0x7f040004;
+        public static final int bitly_password=0x7f040006;
+        public static final int bitly_username=0x7f040005;
+        public static final int error_find_title=0x7f040002;
+        public static final int share_with_title=0x7f040001;
+        public static final int shorten_url=0x7f040003;
+    }
+    public static final class xml {
+        public static final int perference=0x7f030000;
     }
 }
